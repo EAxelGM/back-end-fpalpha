ced8ca56e5acdaf788fd022c38d68e9226f37820
EAxelGMExcite <axel@excite.com.mx>
2023-04-03 23:46:07 -0600

    documentar API con Swagger

--- src/helpers/swagger.js
+++ src/helpers/swagger.js
@@ -2,74 +2,364 @@ const swaggerDocument = {
   openapi: "3.0.1",
   info: {
     version: "1.0.0",
-    title: "APIs Document",
-    description: "your description here",
+    title: "APIs Document Axel Gonzalez",
+    description: "Hola.",
     termsOfService: "",
     contact: {
-      name: "Tran Son hoang",
-      email: "son.hoang01@gmail.com",
-      url: "https://hoangtran.co",
+      name: "Edgar Axel Gonzalez Martinez",
+      email: "axel-canelo@hotmail.com",
+      url: "https://axel-gonzalez.web.app/",
     },
     license: {
       name: "Apache 2.0",
       url: "https://www.apache.org/licenses/LICENSE-2.0.html",
     },
   },
+  schemes: ["http", "https"],
+  consumes: ["application/json"],
+  produces: ["application/json"],
   paths: {
-    "/api/v1/books": {
+    "/api/v1/estadisticas": {
       get: {
-        tags: ["Books"],
-        summary: "Get all books in system",
+        tags: ["Estadisticas"],
+        summary: "Obtienes unas estadisticas de los libros leidos.",
+        parameters: [],
         responses: {
           200: {
-            description: "OK",
+            description: "Success",
             schema: {
-              $ref: "#/definitions/Books",
+              $ref: "#/definitions/Estadisticas",
             },
+          },
+        },
+      },
+    },
+    "/api/v1/books-read": {
+      get: {
+        tags: ["BooksRead"],
+        summary: "Obtener todos los libros leídos.",
+        responses: {
+          200: {
+            description: "Lista de todos los libros leídos.",
+            schema: {
+              type: "array",
+              items: {
+                $ref: "#/definitions/BooksRead",
+              },
+            },
+          },
+        },
+      },
+      post: {
+        tags: ["BooksRead"],
+        summary: "Agregar un libro leído.",
+        parameters: [
+          {
+            name: "book",
+            in: "body",
+            description: "Datos del libro leído.",
+            required: true,
+            schema: {
+              $ref: "#/definitions/BooksRead",
+            },
+          },
+        ],
+        responses: {
+          200: {
+            description: "Libro agregado correctamente.",
+            schema: {
+              $ref: "#/definitions/BooksRead",
+            },
+          },
+        },
+      },
+    },
+    "/api/v1/books-read/{id}": {
+      get: {
+        tags: ["BooksRead"],
+        summary: "Obtener un libro leído por ID.",
+        parameters: [
+          {
+            name: "id",
+            in: "path",
+            description: "ID del libro leído.",
+            required: true,
+            type: "string",
+          },
+        ],
+        responses: {
+          200: {
+            description: "Libro encontrado.",
+            schema: {
+              $ref: "#/definitions/BooksRead",
+            },
+          },
+          404: {
+            description: "Libro no encontrado.",
+          },
+        },
+      },
+      put: {
+        tags: ["BooksRead"],
+        summary: "Actualizar un libro leído por ID.",
+        parameters: [
+          {
+            name: "id",
+            in: "path",
+            description: "ID del libro leído.",
+            required: true,
+            type: "string",
+          },
+          {
+            name: "book",
+            in: "body",
+            description: "Datos actualizados del libro leído.",
+            required: true,
+            schema: {
+              $ref: "#/definitions/BooksRead",
+            },
+          },
+        ],
+        responses: {
+          200: {
+            description: "Libro actualizado correctamente.",
+            schema: {
+              $ref: "#/definitions/BooksRead",
+            },
+          },
+          404: {
+            description: "Libro no encontrado.",
+          },
+        },
+      },
+      delete: {
+        tags: ["BooksRead"],
+        summary: "Eliminar un libro leído por ID.",
+        parameters: [
+          {
+            name: "id",
+            in: "path",
+            description: "ID del libro leído.",
+            required: true,
+            type: "string",
+          },
+        ],
+        responses: {
+          204: {
+            description: "Libro eliminado correctamente.",
+          },
+          404: {
+            description: "Libro no encontrado.",
+          },
+        },
+      },
+    },
+    "/api/v1/books-no-read": {
+      get: {
+        tags: ["BooksNoRead"],
+        summary: "Obtener todos los libros No leídos.",
+        responses: {
+          200: {
+            description: "Lista de todos los libros leídos.",
+            schema: {
+              type: "array",
+              items: {
+                $ref: "#/definitions/BooksNoRead",
+              },
+            },
+          },
+        },
+      },
+      post: {
+        tags: ["BooksNoRead"],
+        summary: "Agregar un libro No leído.",
+        parameters: [
+          {
+            name: "book",
+            in: "body",
+            description: "Datos del libro leído.",
+            required: true,
+            schema: {
+              $ref: "#/definitions/BooksNoRead",
+            },
+          },
+        ],
+        responses: {
+          200: {
+            description: "Libro agregado correctamente.",
+            schema: {
+              $ref: "#/definitions/BooksNoRead",
+            },
+          },
+        },
+      },
+    },
+    "/api/v1/books-no-read/{id}": {
+      get: {
+        tags: ["BooksNoRead"],
+        summary: "Obtener un libro No leído por ID.",
+        parameters: [
+          {
+            name: "id",
+            in: "path",
+            description: "ID del libro leído.",
+            required: true,
+            type: "string",
+          },
+        ],
+        responses: {
+          200: {
+            description: "Libro encontrado.",
+            schema: {
+              $ref: "#/definitions/BooksNoRead",
+            },
+          },
+          404: {
+            description: "Libro no encontrado.",
+          },
+        },
+      },
+      put: {
+        tags: ["BooksNoRead"],
+        summary: "Actualizar un libro No leído por ID.",
+        parameters: [
+          {
+            name: "id",
+            in: "path",
+            description: "ID del libro leído.",
+            required: true,
+            type: "string",
+          },
+          {
+            name: "book",
+            in: "body",
+            description: "Datos actualizados del libro leído.",
+            required: true,
+            schema: {
+              $ref: "#/definitions/BooksNoRead",
+            },
+          },
+        ],
+        responses: {
+          200: {
+            description: "Libro actualizado correctamente.",
+            schema: {
+              $ref: "#/definitions/BooksNoRead",
+            },
+          },
+          404: {
+            description: "Libro no encontrado.",
+          },
+        },
+      },
+      delete: {
+        tags: ["BooksNoRead"],
+        summary: "Eliminar un libro No leído por ID.",
+        parameters: [
+          {
+            name: "id",
+            in: "path",
+            description: "ID del libro No leído.",
+            required: true,
+            type: "string",
+          },
+        ],
+        responses: {
+          204: {
+            description: "Libro eliminado correctamente.",
+          },
+          404: {
+            description: "Libro no encontrado.",
           },
         },
       },
     },
   },
   definitions: {
-    Book: {
-      required: ["name", "_id", "companies"],
+    BooksRead: {
+      type: "object",
       properties: {
-        _id: {
-          type: "string",
-          uniqueItems: true,
-        },
-        /* isPublic: {
-          type: "boolean",
-        }, */
         title: {
           type: "string",
+          description: "Título del libro.",
+          example: "El código Da Vinci",
+          required: true,
         },
-        /* books: {
-          type: "array",
-          items: {
-            type: "object",
-            properties: {
-              name: {
-                type: "string",
-              },
-              amount: {
-                type: "number",
-              },
-            },
+        start_date: {
+          type: "string",
+          format: "date",
+          description: "Fecha de inicio de la lectura.",
+          example: "2022-03-25",
+        },
+        end_date: {
+          type: "string",
+          format: "date",
+          description: "Fecha de finalización de la lectura.",
+          example: "2022-03-29",
+        },
+        qualification: {
+          type: "number",
+          description:
+            "Calificación otorgada al libro en una escala del 1 al 5.",
+          example: 4,
+        },
+        review: {
+          type: "string",
+          description: "Reseña del libro.",
+          example: "Me gustó mucho el libro, es muy interesante.",
+        },
+        id_open_library: {
+          type: "string",
+          description: "ID del libro en Open Library.",
+          example: "OL123456M",
+        },
+        info_open_library: {
+          type: "object",
+          description:
+            "Información adicional del libro obtenida de Open Library.",
+          example: {
+            editorial: ["Editorial XYZ"],
+            pages: 500,
+            authors: "Dan Brown",
           },
         },
-        companies: {
-          type: "array",
-          items: {
-            type: "string",
-          },
-        }, */
       },
     },
-    Books: {
-      type: "array",
-      $ref: "#/definitions/Book",
+    BooksNoRead: {
+      type: "object",
+      properties: {
+        title: {
+          type: "string",
+          description: "Título del libro.",
+          example: "El código Da Vinci",
+          required: true,
+        },
+        id_open_library: {
+          type: "string",
+          description: "ID del libro en Open Library.",
+          example: "OL123456M",
+        },
+        info_open_library: {
+          type: "object",
+          description:
+            "Información adicional del libro obtenida de Open Library.",
+          example: {
+            publishers: ["Editorial XYZ"],
+            pages: 500,
+            authors: "Dan Brown",
+          },
+        },
+      },
+    },
+    Estadisticas: {
+      type: "object",
+      properties: {
+        books_read: { type: "number" },
+        pages_read_total: { type: "number" },
+        pages_read_average: { type: "number" },
+        qualification_total: { type: "number" },
+        qualification_average: { type: "number" },
+      },
     },
   },
 };
